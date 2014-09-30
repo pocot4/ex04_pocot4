d27e52c20cf690b98677c522874fc892bf5cd585
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-30 11:47:48 +0200

    update dr.txt

--- ex04_pocot4/bin/ex04_pocot4_n2.dart
+++ ex04_pocot4/bin/ex04_pocot4_n2.dart
@@ -3,15 +3,15 @@
 import 'dart:math';
 
 main() {
-// convertir degré en rad
- var radian45 = PI * (45 / 180);
- var radian90 = PI * (90 / 180);
- var h = 160;
- var adj = 160 * (sin(radian45) / sin(radian90));//trouver l'adjacent
- var longueure = 800 / 2;
- var hauteur = 600 / 2;
- var positionx = adj + longueure;
- var positiony = adj + hauteur;
- var point = 'x : $positionx y :$positiony';
- print(point);
+  // convertir degré en rad
+  var radian45 = PI * (45 / 180);
+  var radian90 = PI * (90 / 180);
+  var h = 160;
+  var adj = 160 * (sin(radian45) / sin(radian90));//trouver l'adjacent
+  var longueure = 800 / 2;
+  var hauteur = 600 / 2;
+  var positionx = adj + longueure;
+  var positiony = adj + hauteur;
+  var point = 'x : $positionx y :$positiony';
+  print(point);
 }
--- ex04_pocot4/bin/ex04_pocot4_n3.dart
+++ ex04_pocot4/bin/ex04_pocot4_n3.dart
@@ -1,11 +1,10 @@
 //numéro 3: date d'anniversaire
 main() {
-
-DateTime momentpresent = new DateTime.now();
-DateTime fete = DateTime.parse('1993-02-01 12:00:00');
-var annee = (momentpresent.difference(fete).inDays ~/ 365.25);
-var mois = ((momentpresent.difference(fete).inDays % 365.25) ~/(365.25/12));
-var jours = ((momentpresent.difference(fete).inDays % 365.25) - (mois * (365.25/12))).round();
-
-print('J\'ai vecu $annee années, $mois mois et $jours jours.');
+  DateTime momentpresent = new DateTime.now();
+  DateTime fete = DateTime.parse('1993-02-01 12:00:00');
+  var annee = (momentpresent.difference(fete).inDays ~/ 365.25);
+  var mois = ((momentpresent.difference(fete).inDays % 365.25) ~/(365.25/12));
+  var jours = ((momentpresent.difference(fete).inDays % 365.25) - (mois * (365.25/12))).round();
+  
+  print('J\'ai vecu $annee années, $mois mois et $jours jours.');
 }
